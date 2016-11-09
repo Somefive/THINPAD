cf514baf6a8185c0e0742b27d8c18fea56adde4a
Somefive <Somefive@foxmail.com>
2016-11-09 22:57:24 +0800

    THINPAD init

---
+++ Main.vhd
@@ -0,0 +1,81 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    22:35:42 11/09/2016 
+-- Design Name: 
+-- Module Name:    Main - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Main is
+    Port ( FLASH_A : out  STD_LOGIC_VECTOR (22 downto 0);
+           FLASH_D : inout  STD_LOGIC_VECTOR (15 downto 0);
+           SW_DIP : in  STD_LOGIC_VECTOR (15 downto 0);
+           CLK0 : in  STD_LOGIC;
+           CLK1 : in  STD_LOGIC;
+           VGA_B : out  STD_LOGIC_VECTOR (2 downto 0);
+           VGA_G : out  STD_LOGIC_VECTOR (2 downto 0);
+           VGA_R : out  STD_LOGIC_VECTOR (2 downto 0);
+           VGA_HHYNC : out  STD_LOGIC;
+           VGA_VHYNC : out  STD_LOGIC;
+           PS2KB_CLOCK : out  STD_LOGIC;
+           PS2KB_DATA : in  STD_LOGIC;
+           RAM1DATA : inout  STD_LOGIC_VECTOR (15 downto 0);
+           RAM2DATA : inout  STD_LOGIC_VECTOR (15 downto 0);
+           RAM1ADDR : out  STD_LOGIC_VECTOR (17 downto 0);
+           RAM2ADDR : out  STD_LOGIC_VECTOR (17 downto 0);
+           FLASH_BYTE : out  STD_LOGIC;
+           FLASH_CE : out  STD_LOGIC;
+           FLASH_CE1 : out  STD_LOGIC;
+           FLASH_CE2 : out  STD_LOGIC;
+           FLASH_OE : out  STD_LOGIC;
+           FLASH_RP : out  STD_LOGIC;
+           FLASH_STS : out  STD_LOGIC;
+           FLASH_VPEN : out  STD_LOGIC;
+           FLASH_WE : out  STD_LOGIC;
+           U_RXD : out  STD_LOGIC;
+           U_TXD : out  STD_LOGIC;
+           RAM1_EN : out  STD_LOGIC;
+           RAM1_OE : out  STD_LOGIC;
+           RAM1_RW : out  STD_LOGIC;
+           RAM2_EN : out  STD_LOGIC;
+           RAM2_OE : out  STD_LOGIC;
+           RAM2_RW : out  STD_LOGIC;
+           FPGA_LED : out  STD_LOGIC_VECTOR (15 downto 0);
+           DYP0 : out  STD_LOGIC_VECTOR (6 downto 0);
+           DYP1 : out  STD_LOGIC_VECTOR (6 downto 0);
+           CLK_FROM_KEY : in  STD_LOGIC;
+           RESET : in  STD_LOGIC);
+end Main;
+
+architecture Behavioral of Main is
+
+begin
+	process(CLK0)
+	begin
+		DYP0 <= "0000000";
+	end process;
+end Behavioral;
+
